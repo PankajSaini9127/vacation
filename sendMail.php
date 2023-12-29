17de7cf0527142cfb38ddfdf8e62ba97af816cbd
YASH1730 <yashwantsahu3002@gmail.com>
2023-12-29 16:53:51 +0530

    initial

---
+++ sendMail.php
@@ -0,0 +1,137 @@
+<?php
+include('smtp/PHPMailerAutoload.php');
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+	// Read and parse the JSON data sent by the AJAX request
+	$data = json_decode(file_get_contents("php://input"), true);
+
+
+	$TO_EMAIL = 'italianshiv@gmail.com';
+
+
+	$hReferer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "";
+
+
+	if (!empty($_REQUEST['hReferer'])) {
+		$hReferer = $_REQUEST['hReferer'];
+	}
+
+
+	$subject = 'Tour Enquiry through alltimevacations.it (LP)';
+	$subject1 = 'All Time Vacations Tour Enquiry';
+
+
+	$verification = true;
+
+	$tour = isset($_REQUEST['tour']) ? $_REQUEST['tour'] : '';
+	$duration = isset($_REQUEST['duration']) ? $_REQUEST['duration'] : '';
+	$hotel =    isset($_REQUEST['hotel']) ? $_REQUEST['hotel'] : '';
+	$date =       isset($_REQUEST['date']) ? $_REQUEST['date'] : '';
+	$name =     isset($_REQUEST['name']) ? $_REQUEST['name'] : '';
+	$email =    isset($_REQUEST['email']) ? $_REQUEST['email'] : '';
+	$members =   isset($_REQUEST['members']) ? $_REQUEST['members'] : '';
+	$phone =    isset($_REQUEST['phone']) ? $_REQUEST['phone'] : '';
+	$message = isset($_REQUEST['message']) ? $_REQUEST['message'] : 'not provided';
+
+	$previousurl = $_SERVER['HTTP_REFERER'];
+
+	$to1 = $email;
+
+	$body1  = "<html><head><meta name='viewport' content='width=device-width' /><meta http-equiv='Content-Type' content='text/html; charset=UTF-8' /></head><body><table bgcolor='#fafafa' style=' width: 100%!important; height: 100%; background-color: #fafafa; padding: 10px; font-size: 100%; line-height: 1.6;'><tr><td></td><td bgcolor='#FFFFFF' style='border: 1px solid #eeeeee; background-color: #ffffff; border-radius:5px; display:block!important; max-width:600px!important; margin:0 auto!important; clear:both!important;'><div style='padding:10px; max-width:600px; margin:0 auto; display:block;'><table style='width: 100%;'><tr><td rowspan='2'><h2 style='font-weight: 200; font-size: 16px; margin: 5px 0; color: #333333;'>Grazie per averci inviato la Sua richiesta, La contatteremo al piů presto. Noi di All Time Vacations siamo specialisti nell’ organizzare in modo efficiente, ospitale ed indimenticabile i Vostri viaggi. I nostri itinerari innovativi, la nostra estesa rete di agenzie ed i suggerimenti dei clienti che hanno in precedenza viaggiato con noi, ci hanno fatto crescere. Il nostro servizio clienti č attivo 24 su 24, 7 giorni su 7 +91 9560503939, siamo in grado di supportarvi in ogni momento con tutte le informazioni e l'assistenza di cui necessitate</h2></td></tr></table></td></tr><tr><td colspan='2'><p style='text-align: center; display: block; padding-top:20px; font-weight: 700; margin-top:40px; color: #666666; border-top:1px solid #dddddd;font-size:22px;'>All Time Vacation</p></td></tr></table></body></html>";
+	$body = "<html>
+<head>
+<meta name='viewport' content='width=device-width' />
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
+</head>
+<body>
+<table bgcolor='#fafafa' style=' width: 100%!important; height: 100%; background-color: #fafafa; padding: 10px; font-size: 100%; line-height: 1.6;'>
+<tr>
+<td></td>
+<td bgcolor='#FFFFFF' style='border: 1px solid #eeeeee; background-color: #ffffff; border-radius:5px; display:block!important; max-width:600px!important; margin:0 auto!important; clear:both!important;'>
+<div style='padding:10px; max-width:600px; margin:0 auto; display:block;'>
+<table style='width: 100%;'>
+<tr>
+<td colspan='2' style='text-align:center;'>
+<h2 style='font-weight: 800; font-size: 24px; margin: 5px 0; color: #333333;'>Dettagli Del Contatto</h2>
+</td>
+</tr>
+<tr>
+<td><h2 style='font-weight: 200; font-size: 16px; margin: 5px 0; color: #333333;'>Name: <strong>$name</strong></h2>
+</td>
+<td><h2 style='font-weight: 200; font-size: 16px; margin: 5px 0; color: #333333;'>Email: <strong>$email</strong></h2>
+</td>
+</tr>
+<tr>
+<td>
+<h2 style='font-weight: 200; font-size: 16px; margin: 5px 0; color: #333333;'>Phone Number:<strong>$phone</strong></h2>
+</td>
+<td><h2 style='font-weight: 200; font-size: 16px; margin: 5px 0; color: #333333;'>Tour Name : <strong>$tour</strong> </h2>
+</td>
+</tr>
+<tr>
+<td><h2 style='font-weight: 200; font-size: 16px; margin: 5px 0; color: #333333;'>Date of Travel: <strong>$date </strong></h2>
+</td>
+<td>
+<h2 style='font-weight: 200; font-size: 16px; margin: 5px 0; color: #333333;'>Duration of Trip : <strong>$duration</strong> </h2>
+</td>
+</tr>
+<tr>
+<td>
+<h2 style='font-weight: 200; font-size: 16px; margin: 5px 0; color: #333333;'>Number of Guest:<strong>$members</strong> 
+</td>
+<td>
+<h2 style='font-weight: 200; font-size: 16px; margin: 5px 0; color: #333333;'>Hotel: <strong>$hotel</strong></h2>
+</td>
+</tr>
+<tr>
+<td colspan='2'><h2 style='font-weight: 200; font-size: 16px; margin: 5px 0; color: #333333;'>Requirements:<strong>$message</strong></h2>
+</td>
+</tr>
+<tr>
+<td><h2 style='font-weight: 200; font-size: 16px; margin: 5px 0; color: #333333;'>Referral url :<strong>$hReferer</strong></h2>
+</td>
+</tr>
+<tr><td colspan='2'><p style='text-align: center; display: block; padding-top:20px; font-weight: 700; margin-top:40px; color: #666666; border-top:1px solid #dddddd;font-size:22px;'>All Time Vacation</p></td></tr></table></div></td></tr></table></body></html>";
+
+
+
+	if ($verification) {
+		$mail = new PHPMailer();
+		$mail->IsSMTP();
+
+		$mail->Host = 'alltimevacations.it';
+		$mail->Port = 25;
+		$mail->SMTPAuth = false;
+		$mail->SMTPSecure = false;
+		$mail->Username = "italy@alltimevacations.it";
+		$mail->Password = "Italy@123";
+		$mail->SetFrom("italy@alltimevacations.it");
+		$mail->IsHTML(true);
+		$mail->CharSet = 'UTF-8';
+		$mail->FromName = 'All time vacations';
+		$mail->Subject = $subject;
+		$mail->Body = $body;
+		$mail->AddAddress($TO_EMAIL);
+		$mail->addCC('johnsonn.thomas@googlemail.com');
+		$mail->SMTPOptions = array('ssl' => array(
+			'verify_peer' => false,
+			'verify_peer_name' => false,
+			'allow_self_signed' => false
+		));
+
+
+		if (!$mail->Send()) {
+			echo $mail->ErrorInfo;
+		} else {
+			$mail->ClearAllRecipients();
+			$mail->addAddress($to1);
+			$mail->Subject = $subject1;
+			$mail->Body = $body1;
+			$mail->send();
+			return true;
+		}
+	} else {
+		header("location:{$previousurl}");
+	}
+}
+?>
---
+++ thankuPage.php
@@ -0,0 +1,110 @@
+<!doctype html>
+<html lang=en>
+<head>
+<meta charset=utf-8>
+<meta http-equiv=X-UA-Compatible content="IE=edge">
+<meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0">
+<title>Viaggio in India | Viaggi India</title>
+<link rel=stylesheet href=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin=anonymous referrerpolicy=no-referrer>
+<link rel=apple-touch-icon sizes=180x180 href=assets/images/favicon_io/apple-touch-icon.png style=height:60px>
+<link rel=icon type=image/png sizes=32x32 href=assets/images/favicon_io/favicon-32x32.png>
+<link rel=icon type=image/png sizes=16x16 href=assets/images/favicon_io/favicon-16x16.png>
+<link rel=manifest href=assets/images/favicon_io/site.webmanifest>
+<link href="../../fonts.googleapis.com/css2c52e.css?family=Amatic+SC:wght@400;700&amp;display=swap" rel=stylesheet>
+<link href="../../fonts.googleapis.com/css2c305.css?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel=stylesheet>
+<link href=assets/css/font-awesome-all.css rel=stylesheet>
+<link href=assets/css/flaticon.css rel=stylesheet>
+<link href=assets/css/owl.css rel=stylesheet>
+<link href=assets/css/bootstrap.css rel=stylesheet>
+<link href=assets/css/jquery.fancybox.min.css rel=stylesheet>
+<link href=assets/css/animate.css rel=stylesheet>
+<link href=assets/css/color.css rel=stylesheet>
+<link href=assets/css/style.css rel=stylesheet>
+<link href=assets/css/responsive.css rel=stylesheet>
+<link href=assets/css/header.css rel=stylesheet>
+<link href=assets/css/change.css rel=stylesheet>
+<link rel=stylesheet href=assets/css/preloader.css>
+<link href=https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css rel=stylesheet integrity=sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9 crossorigin=anonymous>
+<script defer  src="https://www.googletagmanager.com/gtag/js?id=AW-11360329254"></script>
+<script defer>function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","AW-11360329254")</script>
+<script defer>gtag("event","conversion",{send_to:"AW-11360329254/05NYCN2FsugYEKa8g6kq"})</script>
+</head>
+<body>
+<div class=preloader>
+<div class=boxes>
+<div class=box>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+</div>
+<div class=box>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+</div>
+<div class=box>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+</div>
+<div class=box>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+</div>
+</div>
+</div>
+<div class=thank-you-section>
+<?php include './header.php'; ?>
+<section style=padding-top:90px>
+<div class=container-fluid>
+<div class="row justify-content-center align-items-center">
+<div class="col-lg-6 col-md-8 col-sm-12 col-12 p-0 contact-image">
+<figure class=m-0>
+<image src=assets/images/about-india-tourism.webp alt="" class="image-fluid img-responsive" style=height:90vh;width:100%>
+</image></figure>
+</div>
+<div class="col-lg-6 col-md-8 col-sm-12 col-12 p-0">
+<div class="popup px-3">
+<svg width=140px height=140px viewBox="0 0 1024.00 1024.00" class=icon version=1.1 xmlns=http://www.w3.org/2000/svg fill=#fff stroke=#fff stroke-width=6.144>
+<g id=SVGRepo_bgCarrier stroke-width=0></g>
+<g id=SVGRepo_tracerCarrier stroke-linecap=round stroke-linejoin=round></g>
+<g id=SVGRepo_iconCarrier>
+<path d="M512 512m-448 0a448 448 0 1 0 896 0 448 448 0 1 0-896 0Z" fill=#47b105></path>
+<path d="M738.133333 311.466667L448 601.6l-119.466667-119.466667-59.733333 59.733334 179.2 179.2 349.866667-349.866667z" fill=#47b1051588c5></path>
+</g>
+</svg>
+<p class="text-center display-6">Grazie per averci inviato la Sua richiesta</p>
+<p class="text-center text-secondary">La contatteremo al più presto. Noi di All Time Vacations siamo specialisti nell’ organizzare in modo efficiente, ospitale ed indimenticabile i Vostri viaggi. I nostri itinerari innovativi, la nostra estesa rete di agenzie ed i suggerimenti dei clienti che hanno in precedenza viaggiato con noi, ci hanno fatto crescere. Il nostro servizio clienti è attivo 24 su 24, 7 giorni su 7 +39 351 8664836, siamo in grado di supportarvi in ogni momento con tutte le informazioni e l'assistenza di cui necessitate. Cordiali Saluti,<br>
+All Time Vacations</p>
+<a href=index.php><button class="btn read-more-btn book-btn theme-btn btn-sm p-1 mx-auto d-block"> Back</button>
+</a>
+</div>
+</div>
+</div>
+</div>
+</section>
+</div>
+<button class="scroll-top scroll-to-target" data-target="html ">
+<i class="fal fa-long-arrow-up"></i>
+</button>
+</body>
+<script defer src=assets/js/jquery.js></script>
+<script defer src=assets/js/popper.min.js></script>
+<script defer src=assets/js/bootstrap.min.js></script>
+<script defer src=assets/js/owl.js></script>
+<script defer src=assets/js/wow.js></script>
+<script defer src=assets/js/validation.js></script>
+<script defer src=assets/js/jquery.fancybox.js></script>
+<script defer src=assets/js/appear.js></script>
+<script defer src=assets/js/scrollbar.js></script>
+<script defer src=assets/js/parallax-scroll.js></script>
+<script defer src=assets/js/isotope.js></script>
+<script defer src=assets/js/script.js></script>
+<script defer src=assets/js/carForm.js></script>
+<script defer src=https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm " crossorigin="anonymous "></script>
+</html>
